2f31e6183e14ed6b57e8f49c11d1a0df692fb2b0
eubank87 <eubank.ashley@gmail.com>
2021-01-27 17:43:18 -0800

    changed main display to all thre conatiners

--- script.js
+++ script.js
@@ -15,21 +15,21 @@ $(document).ready(() => {
     //     console.log('clicked')
     // })
 
-    $(document).on('click', '.nav-link', function (event) {
-        event.preventDefault()
+    // $(document).on('click', '.nav-link', function (event) {
+    //     event.preventDefault()
 
-        loadPage = $(this).text().trim().toLowerCase()
-        console.log(loadPage)
+    //     loadPage = $(this).text().trim().toLowerCase()
+    //     console.log(loadPage)
 
-        if (loadPage === 'ashley eubank') {
-            loadPage = 'about'
-        }
+    //     if (loadPage === 'ashley eubank') {
+    //         loadPage = 'about'
+    //     }
 
-        $(`.${currentPage}-container`).fadeOut(function () {
-            $(`.${loadPage}-container`).fadeIn()
-            currentPage = loadPage
-        })
-    })
+    //     $(`.${currentPage}-container`).fadeOut(function () {
+    //         $(`.${loadPage}-container`).fadeIn()
+    //         currentPage = loadPage
+    //     })
+    // })
 
 
     startPage();
